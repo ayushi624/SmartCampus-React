0a49be4f33333d35eaffc67bf7078f86455180c3
Ishvika <ishvika1175.becse24@chitkara.edu.in>
2025-11-18 12:41:22 +0530

    did some changes

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../components/Sidebar';
 import Topbar from '../components/Topbar';
-import '../assets/index.css'; // Main stylesheet
+import '../assets/index.css'; 
 
 function Layout({role, userName}) {
   const [searchQuery, setSearchQuery] = useState('');
 
   return (
     <div className="app">
-      <Sidebar userType={role === 'TEACHER' ? 'teacher' : 'student'} /> {/* The only change is here */}
+      <Sidebar userType={role === 'TEACHER' ? 'teacher' : 'student'} /> 
       <main>
         <Topbar
   searchQuery={searchQuery}
